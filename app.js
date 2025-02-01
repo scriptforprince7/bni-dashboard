9a84574cdf6b33bba3e0014821656a4fe8b3b963
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2025-02-01 14:51:07 +0530

    env

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-require('dotenv').config(); // Load environment variables from .env file
-const apiRoutes = require('./routes/r-api/apiRoutes'); // Adjust the path as needed
-const express = require('express');
-const path = require('path');
-const routes = require('./routes'); // Import all routes from index.js
+require("dotenv").config();
+const apiRoutes = require("./routes/r-api/apiRoutes");
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
 
 const app = express();
 
 // Set custom views directory
-app.set('views', path.join(__dirname, 'public/view'));
+app.set("views", path.join(__dirname, "public/view"));
 
 // Set the view engine to EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-// Serve static files like CSS or images from the 'public' directory
-app.use(express.static('public'));
+// Serve static files
+app.use(express.static("public"));
 
-// Use the consolidated routes
-app.use('/', routes); // All routes are prefixed with /api
+// Middleware to set base_url globally
+app.use((req, res, next) => {
+  res.locals.base_url = process.env.BASE_URL; // For EJS templates
+  req.base_url = process.env.BASE_URL; // For routes
+  next();
+});
 
-app.use('/api', apiRoutes); // This will handle routes starting with /api
+// Use general routes
+app.use("/", routes);
+
+// Use API routes
+app.use("/api", apiRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Base URL is set to ${process.env.BASE_URL}`);
 });
