0a445bfc6792e82ece0bb74c0ff30f7da010d858
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2025-02-01 19:01:17 +0530

    env9

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Import all routes
-const indexRoutes = require('./r-home/indexRoutes');
-const memberRoutes = require('./r-member/memberRoutes');
-const regionRoutes = require('./r-region/regionRoutes');
-const chapterRoutes = require('./r-chapter/chapterRoutes');
-const dashboardRoutes = require('./r-dashboard/dashboardRoutes');
-const universalRoutes = require('./r-unversalLinks/universalLinksRoutes');
-const transactionRoutes = require('./r-transactions/transactionsRoutes');
-const apiRoutes = require('./r-api/apiRoutes');
-const eInvoiceRoutes = require('./r-eInvoice/eInvoiceRoutes');
-const authRoutes = require('./r-authentication/authenticationRoutes')
-const accoladesRoutes = require('./r-accolades/accoladesRoutes')
-const settingRoutes = require('./r-settings/settingsRoutes')
-const classificationRoutes = require('./r-classifications/classificationRoutes')
-const eventsRoutes = require('./r-events/eventsRoutes')
-const trainingsRoutes = require('./r-trainings/trainingRoutes')
-const kittyDashboardRoutes = require('./r-kittyDashboard/kittyDashboardRoutes')
-const expenseRoutes=require('./r-expenses/expenseRoutes')
-const memberAccoladesRoutes = require('./r-memberAccolades/memberAccolades')
-const memberTrainingsRoutes = require('./r-memberTraining/memberTrainings')
-const memberInvoiceRoutes = require('./r-memberInvoice/memberInvoice')
-const refundRoutes = require('./r-refund/refundRoutes')
-const chapterSettingsRoutes = require('./r-settings/settingsRoutes');
-const chapterMembersRoutes = require('./r-chapterMembers/chapterMembers');
-const chapterKitty = require('./r-chapterKitty/chapterKitty')
-const sendNotificationRoutes = require('./r-sendNotification/sendNotificationRoutes');
+const indexRoutes = require("./r-home/indexRoutes");
+const memberRoutes = require("./r-member/memberRoutes");
+const regionRoutes = require("./r-region/regionRoutes");
+const chapterRoutes = require("./r-chapter/chapterRoutes");
+const dashboardRoutes = require("./r-dashboard/dashboardRoutes");
+const universalRoutes = require("./r-unversalLinks/universalLinksRoutes");
+const transactionRoutes = require("./r-transactions/transactionsRoutes");
+const apiRoutes = require("./r-api/apiRoutes");
+const eInvoiceRoutes = require("./r-eInvoice/eInvoiceRoutes");
+const authRoutes = require("./r-authentication/authenticationRoutes");
+const accoladesRoutes = require("./r-accolades/accoladesRoutes");
+const settingRoutes = require("./r-settings/settingsRoutes");
+const classificationRoutes = require("./r-classifications/classificationRoutes");
+const eventsRoutes = require("./r-events/eventsRoutes");
+const trainingsRoutes = require("./r-trainings/trainingRoutes");
+const kittyDashboardRoutes = require("./r-kittyDashboard/kittyDashboardRoutes");
+const expenseRoutes = require("./r-expenses/expenseRoutes");
+const memberAccoladesRoutes = require("./r-memberAccolades/memberAccolades");
+const memberTrainingsRoutes = require("./r-memberTraining/memberTrainings");
+const memberInvoiceRoutes = require("./r-memberInvoice/memberInvoice");
+const refundRoutes = require("./r-refund/refundRoutes");
+const chapterSettingsRoutes = require("./r-settings/settingsRoutes");
+const chapterMembersRoutes = require("./r-chapterMembers/chapterMembers");
+const chapterKitty = require("./r-chapterKitty/chapterKitty");
+const sendNotificationRoutes = require("./r-sendNotification/sendNotificationRoutes");
 
 // Use all the imported routes
-router.use('/', indexRoutes);
-router.use('/d', dashboardRoutes);
-router.use('/m', memberRoutes);
-router.use('/r', regionRoutes);
-router.use('/c', chapterRoutes);
-router.use('/u', universalRoutes);
-router.use('/t', transactionRoutes);
-router.use('/a', apiRoutes);
-router.use('/v', eInvoiceRoutes);
-router.use('/auth', authRoutes);
-router.use('/acc', accoladesRoutes);
-router.use('/macc', memberAccoladesRoutes);
-router.use('/s', settingRoutes);
-router.use('/cl', classificationRoutes);
-router.use('/e', eventsRoutes);
-router.use('/tr', trainingsRoutes);
-router.use('/k', kittyDashboardRoutes);
-router .use('/exp', expenseRoutes);
-router .use('/mtr', memberTrainingsRoutes);
-router .use('/minv', memberInvoiceRoutes);
-router.use('/ref', refundRoutes);
+router.use("dashboard/", indexRoutes);
+router.use("dashboard/d", dashboardRoutes);
+router.use("dashboard/m", memberRoutes);
+router.use("dashboard/r", regionRoutes);
+router.use("dashboard/c", chapterRoutes);
+router.use("dashboard/u", universalRoutes);
+router.use("dashboard/t", transactionRoutes);
+router.use("dashboard/a", apiRoutes);
+router.use("dashboard/v", eInvoiceRoutes);
+router.use("dashboard/auth", authRoutes);
+router.use("dashboard/acc", accoladesRoutes);
+router.use("dashboard/macc", memberAccoladesRoutes);
+router.use("dashboard/s", settingRoutes);
+router.use("dashboard/cl", classificationRoutes);
+router.use("dashboard/e", eventsRoutes);
+router.use("dashboard/tr", trainingsRoutes);
+router.use("dashboard/k", kittyDashboardRoutes);
+router.use("dashboard/exp", expenseRoutes);
+router.use("dashboard/mtr", memberTrainingsRoutes);
+router.use("dashboard/minv", memberInvoiceRoutes);
+router.use("dashboard/ref", refundRoutes);
 
-router.use('/cm', chapterMembersRoutes);
-router.use('/ck', chapterKitty);
-router.use('/sn', sendNotificationRoutes);
+router.use("dashboard/cm", chapterMembersRoutes);
+router.use("dashboard/ck", chapterKitty);
+router.use("dashboard/sn", sendNotificationRoutes);
 module.exports = router;
