edb449950f49f1f4aca5199c7413a958fc0f7df2
Prince Sachdeva <scriptforprince@gmail.com>
2025-04-16 16:29:21 +0530

    Link correct for member write off

--- public/assets/js/chapterMemberRightoff.js
+++ public/assets/js/chapterMemberRightoff.js
@@ -300,7 +300,7 @@ fetch(getBankOrderApi)
                 console.log('Sending data to API:', data);
         
                 showLoader();
-                const response = await fetch('backend.bninewdelhi.com/api/addMemberWriteOff', {
+                const response = await fetch('https://backend.bninewdelhi.com/api/addMemberWriteOff', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
